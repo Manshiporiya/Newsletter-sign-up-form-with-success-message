e27eb79b90b46eb30122a3338adea6c66d1336c3
Manshiporiya <manshiporiya04@gmail.com>
2024-12-23 16:43:47 +0530

    first commit

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-const email=document.getElementById("email").addEventListener("submit", function (e) {
+const emailForm = document.getElementById("email").addEventListener("submit", function (e) {
     e.preventDefault();
-
+  
     const emailInput = document.getElementById("email").querySelector("input");
-    const emailValue = emailInput.value;
+    const emailValue = emailInput.value.trim();
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    const errorLabel = document.createElement("p");
-    errorLabel.textContent = "Valid email required";
-    errorLabel.style.color = "red";
-
-
+  
     if (emailInput.nextSibling) {
-        emailInput.nextSibling.remove();
+      emailInput.nextSibling.remove();
     }
+  
     if (emailRegex.test(emailValue)) {
-        document.querySelector(".container").style.display = "none";
-        document.querySelector(".message-container").style.display = "block";
-
-        emailInput.style.border = "1px solid #ccc";
-    } else {
-
-        emailInput.style.border = "1px solid red";
-        emailInput.parentNode.insertBefore(errorLabel, emailInput.nextSibling); 
+    
+      document.querySelector(".message-container").style.display = "block";
+      document.querySelector(".container").style.display = "none";
+  
+      const emailDisplay = document.querySelector("#messageContainer strong");
+      emailDisplay.textContent = emailValue;
+  
+      emailInput.style.border = "1px solid #ccc";
+    } 
+    else {
+      const errorLabel = document.createElement("p");
+      errorLabel.textContent = "Valid email required";
+      errorLabel.style.color = "red";
+  
+      emailInput.style.border = "1px solid red";
+      emailInput.parentNode.insertBefore(errorLabel, emailInput.nextSibling);
     }
-});
-
-
+  });
+  
